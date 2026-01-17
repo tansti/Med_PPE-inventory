92278036c13c2decb278a8893bd609cd37ba1cb3
tansti <tansti30@gmail.com>
2026-01-17 18:19:13 +0800

    app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
-
+ 
 /* ================= GLOBAL STATE ================= */
 let allLogs = [];
 let lowStockItems = [];
